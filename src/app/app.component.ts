533a8a3ee14cefa31a13b5885a11b4f83a6e048f
Abhi <aanand@argusoft.com>
2023-10-23 18:42:07 +0530

    validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-forms';
+[x: string]: any;
+
+  constructor(private fb: FormBuilder){}
+
+  registrationForm=this.fb.group({
+    userName: ['', [Validators.required, Validators.minLength(3)]],
+    password:[''],
+    confirmPassword:[''],
+    address: this.fb.group({
+      city:[''],
+      state:[''],
+      postalCode:['']
+    })
+  })
+  
+  
+
+
+  // registrationForm=new FormGroup({
+  //   userName: new FormControl('Sabuja'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl(''),
+  //   })
+
+  // });
+
+  loadApiData(){
+    this.registrationForm.setValue({
+      userName: 'Swapnil',
+      password: 'test',
+      confirmPassword: 'test',
+      address: {
+        city: 'Indore',
+        state: 'MP',
+        postalCode: '123456'
+      }
+    });
+  }
 }
